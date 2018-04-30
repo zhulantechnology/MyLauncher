35896f15fe4411e9f7c7723582a627c6621d1923
Jeffreywangjun <wangjunahasd@163.com>
2018-04-30 10:23:14 +0800

    draw the deskyop UI,主界面布局初步
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/ds05/mylauncher/ui/home/DesktopFragment.java
+++ app/src/main/java/com/ds05/mylauncher/ui/home/DesktopFragment.java
@@ -2,26 +2,36 @@ package com.ds05.mylauncher.ui.home;
 
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ds05.mylauncher.R;
 
-public class DesktopFragment extends BaseFragment {
+public class DesktopFragment extends Fragment {
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.desktop_frag, container,false);
         return rootView;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        Fragment fragment = manager.findFragmentById(R.id.fragment);
+        if (fragment == null) {
+            transaction.add(R.id.fragment, new HomeDisplayFragment());
+            transaction.commit();
+        }
+
     }
 }
---
+++ app/src/main/java/com/ds05/mylauncher/ui/home/HomeDisplayFragment.java
@@ -0,0 +1,19 @@
+package com.ds05.mylauncher.ui.home;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ds05.mylauncher.R;
+
+public class HomeDisplayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home_display_fragment, container, false);
+        return rootView;
+    }
+}
