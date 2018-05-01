392c378f19cf540baea346b50f5511cdc8a2d088
Jeffreywangjun <wangjunahasd@163.com>
2018-05-01 10:06:46 +0800

    round corner review, 圆角效果小结
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/ds05/mylauncher/view/CornerImageView.java
+++ app/src/main/java/com/ds05/mylauncher/view/CornerImageView.java
@@ -24,17 +24,17 @@ public class CornerImageView extends ImageView {
 
     public CornerImageView(Context context) {
         super(context);
-        Log.e("XXX", "WANGJUN-------------1111");
+        //Log.e("XXX", "WANGJUN-------------1111");
     }
 
     public CornerImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        Log.e("XXX", "WANGJUN-------------2222");
+       // Log.e("XXX", "WANGJUN-------------2222");
     }
 
     public CornerImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        Log.e("XXX", "WANGJUN-------------3333");
+       // Log.e("XXX", "WANGJUN-------------3333");
 
     }
 
@@ -46,6 +46,8 @@ public class CornerImageView extends ImageView {
             Bitmap b = getRoundBitmap(bitmap, 20);
             final Rect rectSrc = new Rect(0, 0, b.getWidth(), b.getHeight());
             final Rect rectDest = new Rect(0,0, getWidth(), getHeight());
+           // Log.e("XXX", "WANGJUN-----b.getWidth():" + b.getWidth()); // 550 图片的宽度
+           // Log.e("XXX", "WANGJUN-------getWidth():" + getWidth()); // 989 此view所占的宽度
             paint.reset();
             canvas.drawBitmap(b, rectSrc, rectDest, paint);
         } else {
@@ -66,11 +68,13 @@ public class CornerImageView extends ImageView {
         paint.setAntiAlias(true);
         canvas.setDrawFilter(new PaintFlagsDrawFilter(0,
                 Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG));
-        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawARGB(0, 0, 0, 0); // 填充整个画布
         paint.setColor(color);
 
         canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        // 此处的PorterDuff.SRC_IN实现的圆角效果！
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        // 此处只是改变bitmap的圆角效果，drawBitmap的src和des是一样的。
         canvas.drawBitmap(bitmap, rect, rect, paint);
         return output;
     }
