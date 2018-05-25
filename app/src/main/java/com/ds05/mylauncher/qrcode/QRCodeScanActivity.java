c59456cef4b62166d06a3455b10b777d7a242de6
Jeffreywangjun <wangjunahasd@163.com>
2018-05-25 14:09:28 +0800

    QRcode function works
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/ds05/mylauncher/qrcode/QRCodeScanActivity.java
+++ app/src/main/java/com/ds05/mylauncher/qrcode/QRCodeScanActivity.java
@@ -1,12 +1,34 @@
 package com.ds05.mylauncher.qrcode;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.text.TextUtils;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
+import com.ds05.mylauncher.LauncherApplication;
 import com.ds05.mylauncher.R;
+import com.ds05.mylauncher.qrcode.camera.CameraManager;
+import com.ds05.mylauncher.qrcode.decoding.CaptureActivityHandler;
+import com.ds05.mylauncher.qrcode.decoding.InactivityTimer;
+import com.ds05.mylauncher.qrcode.view.ViewfinderView;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
 
-public class QRCodeScanActivity extends Activity {
+import java.io.IOException;
+import java.util.Vector;
+
+public class QRCodeScanActivity extends Activity implements SurfaceHolder.Callback{
 
     public static final int ACT_REQUEST_CODE_GET_WIFI = 1;
     public static final String EXTRA_QRCODE_RESULT = "QRCode-Result";
@@ -15,12 +37,266 @@ public class QRCodeScanActivity extends Activity {
     public static final int REASON_NONE = -1;
     public static final int REASON_GET_WIFI = 0;
 
+    private CaptureActivityHandler handler;
+    private ViewfinderView viewfinderView;
+    private boolean hasSurface;
+    private Vector<BarcodeFormat> decodeFormats;
+    private String characterSet;
+    private InactivityTimer inactivityTimer;
+    private MediaPlayer mediaPlayer;
+    private boolean playBeep;
+    private static final float BEEP_VOLUME = 0.10f;
+    private boolean vibrate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qrcode_layout);
 
         CameraManager.init(getApplication());
+        viewfinderView = findViewById(R.id.viewfinder_view);
+        hasSurface = false;
+        inactivityTimer = new InactivityTimer(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            initCamera(surfaceHolder);
+        } else {
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        decodeFormats = null;
+        characterSet = null;
+        playBeep = true;
+
+        AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
+        if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+            playBeep = false;
+        }
+        initBeepSound();
+        vibrate = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        CameraManager.get().closeDriver();
+    }
+
+    @Override
+    protected void onDestroy() {
+        inactivityTimer.shutdown();
+        super.onDestroy();
+    }
+
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        try {
+            CameraManager.get().openDriver(0,surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            return;
+        }
+
+        if (handler == null) {
+            handler = new CaptureActivityHandler(this, decodeFormats, characterSet);
+        }
+    }
+
+    private void initBeepSound() {
+        if (playBeep && mediaPlayer == null) {
+            // The volume on STREAM_SYSTEM is not adjustable, and users found it
+            // too loud,
+            // so we now play on the music stream.
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setOnCompletionListener(beepListener);
+
+            AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.beep);
+            try {
+                mediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
+                file.close();
+                mediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                mediaPlayer = null;
+            }
+        }
+    }
+
+    private static final long VIBRATE_DURATION = 200L;
+
+    private void playBeepSoundAndVibrate() {
+        if (playBeep && mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+        if (vibrate) {
+            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            vibrator.vibrate(VIBRATE_DURATION);
+        }
+    }
+
+    /**
+     * When the beep has finished playing, rewind to queue up another one.
+     */
+    private final MediaPlayer.OnCompletionListener beepListener = new MediaPlayer.OnCompletionListener() {
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.seekTo(0);
+        }
+    };
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    public ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+
+    public CaptureActivityHandler getHandler() {
+        return handler;
+    }
+
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+    }
+
+    public void handleDecode(final Result obj, Bitmap barcode) {
+        inactivityTimer.onActivity();
+        playBeepSoundAndVibrate();
+
+        int reqReason = getIntent().getIntExtra(EXTRA_REQ_REASON, REASON_NONE);
+        switch (reqReason) {
+            case REASON_GET_WIFI: {
+                if(obj == null && TextUtils.isEmpty(obj.getText())) {
+                    setResult(RESULT_CANCELED);
+                    showDialog(obj, barcode);
+                    break;
+                }
+                String[] wifiInfo = obj.getText().split(",");
+                if (wifiInfo.length == 2 || wifiInfo.length == 3) {
+                    Intent intent = new Intent();
+                    intent.putExtra(EXTRA_QRCODE_RESULT, obj.getText());
+                    setResult(RESULT_OK, intent);
+
+                    finish();
+                    break;
+                } else {
+                    setResult(RESULT_CANCELED);
+                    showDialog(obj, barcode);
+                    break;
+                }
+            }
+            default:
+                showDialog(obj, barcode);
+        }
+    }
+
+    private void showDialog(final Result obj, Bitmap barcode) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        if (barcode == null) {
+            dialog.setIcon(null);
+        } else {
+            Drawable drawable = new BitmapDrawable(barcode);
+            dialog.setIcon(drawable);
+        }
+        dialog.setTitle(R.string.settings_str_scan_qr_code_result);
+        dialog.setMessage(obj.getText());
+        dialog.setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+//                Intent intent = new Intent();
+//                intent.setAction("android.intent.action.VIEW");
+//                Uri content_url = Uri.parse(obj.getText());
+//                intent.setData(content_url);
+//                startActivity(intent);
+                //vincent add start
+                Intent intent = new Intent();
+                intent.setAction("com.ds05.Broadcast.ToServer.NOTIFY_QRCODE_RESULT");
+                String [] arrs=obj.getText().split(",");
+                intent.putExtra("QRCodeResult_UserId",arrs[0]);
+                intent.putExtra("QRCodeResult_WifiSSID",arrs[1]);
+                intent.putExtra("QRCodeResult_WifiPassword",arrs[2]);
+                // Uri content_url = Uri.parse(obj.getText());
+                // intent.setData(content_url);
+                LauncherApplication.getContext().sendBroadcast(intent);
+                //vincent add end
+                finish();
+            }
+        });
+        dialog.setPositiveButton(android.R.string.cancel,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        dialog.create().show();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/ds05/mylauncher/ui/setting/SettingsFragment.java
+++ app/src/main/java/com/ds05/mylauncher/ui/setting/SettingsFragment.java
@@ -1,13 +1,17 @@
 package com.ds05.mylauncher.ui.setting;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
 import com.ds05.mylauncher.ModuleBaseFragment;
 import com.ds05.mylauncher.R;
+import com.ds05.mylauncher.qrcode.QRCodeScanActivity;
 
 import java.util.Locale;
 
@@ -15,9 +19,12 @@ import java.util.Locale;
  * Created by Jun.wang on 2018/5/2.
  */
 
-public class SettingsFragment extends ModuleBaseFragment {
+public class SettingsFragment extends ModuleBaseFragment
+                implements Preference.OnPreferenceChangeListener{
 
     public static final String KEY_LANGUAGE = "key_Settings_language";
+    public static final String KEY_QR_CODE = "key_settings_scan_qr_code";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +61,24 @@ public class SettingsFragment extends ModuleBaseFragment {
         langPref.setValue("" + index);
         langPref.setSummary(langPref.getEntries()[index]);
         //langPref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        String key = preference.getKey();
+        if (key.equals(KEY_QR_CODE)) {
+            Intent intent = new Intent(getActivity(), QRCodeScanActivity.class);
+            intent.putExtra(QRCodeScanActivity.EXTRA_REQ_REASON, QRCodeScanActivity.REASON_GET_WIFI);
+            startActivityForResult(intent, QRCodeScanActivity.ACT_REQUEST_CODE_GET_WIFI);
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
     }
 }
 
